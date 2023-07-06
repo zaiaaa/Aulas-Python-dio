953215c81faaa727abee620b9b42d2d101b1d19e
zaiaaa <gustavo.0801214@gmail.com>
2023-07-06 14:21:19 -0300

    oi

---
+++ tipos de operadores/operadores_identidade.py
@@ -0,0 +1,11 @@
+curso = "Curso de Python"
+nome_curso = curso
+saldo, limite = 200, 200 
+
+#-----curso == nome_curso
+print(curso is nome_curso)
+
+#---- curso != nome_curso
+print(curso is not nome_curso)
+#------ saldo == limite
+print(saldo is limite)
