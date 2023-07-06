11191b19a0c8870fbd81b30f2aa61f42fdd05aa0
zaiaaa <gustavo.0801214@gmail.com>
2023-07-06 14:41:06 -0300

    operadores_associacao

---
+++ tipos de operadores/operadores_associacao.py
@@ -0,0 +1,12 @@
+curso = "Curso de Python"
+frutas = ["laranja", "uva", "limão"]
+saques = [1500, 100]
+
+# verifica se existe tal coisa em tal variável (letras maiúsculas ou minúsculas importam na consulta)
+verify = "Python" in curso
+print(verify)
+
+verify_frutas = "maçã" not in frutas
+print(verify_frutas)
+
+200 in saques
