cc13adf5ab97f6b8e6a95f172f1faa806315ab60
zaiaaa <gustavo.0801214@gmail.com>
2023-07-10 23:09:20 -0300

    interessante

---
+++ manipulando strings/fatiamento_de_strings.py
@@ -0,0 +1,22 @@
+nome = "Guilherme Arthur"
+numero = str(12309)
+#sim temos que transformar o numero em string pra poder manipular ele assim
+
+#pega a última letra
+print(nome[-1])
+print(numero[-1])
+
+#pega e exibe do indice 0 até o 9
+print(nome[:9])
+
+#pega e exibe do indice 10 pra frente
+print(nome[10:])
+
+#pega e exibe do indice 10 ao 16
+print(nome[10:16])
+
+#pega e exibe do indice 10 ao 16 com step em 2 (pega de 2 em 2))
+print(nome[10:16:2])
+
+#escreve ao contrário
+print(nome[::-1])
