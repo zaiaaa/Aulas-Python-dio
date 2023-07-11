a943d4fb7b1de3a7da11cfa9ec1a35ced037a385
zaiaaa <gustavo.0801214@gmail.com>
2023-07-10 23:18:12 -0300

    legal

---
+++ manipulando strings/strings_multiplas_linhas.py
@@ -0,0 +1,11 @@
+nome = 'Guilherme'
+
+# é como se fosse as `` de JS, preserva sua formatação
+mensagem = f"""
+Olá meu nome é {nome},
+Eu estou aprendendo Python
+awgawg
+awgawgwga
+"""
+
+print(mensagem)
