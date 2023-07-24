2b07a1686776a196ab7195a3f9d0131df690f755
zaiaaa <gustavo.0801214@gmail.com>
2023-07-24 10:31:56 -0300

    tuplas imutáveis

---
+++ listas/tuplas.py
@@ -0,0 +1,28 @@
+# Tupla é uma lista constante
+
+frutas = (
+    "laranja",
+    "pera",
+    "uva",
+)
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+
+numeros = tuple([1, 2, 3, 4])
+print(numeros)
+
+pais = ("Brasil",)
+print(pais)
+
+matriz = (
+    (1, "a", 2),
+    ("b", 3, 4),
+    (6, 5, "c"),
+)
+
+print(matriz[0])  # (1, "a", 2)
+print(matriz[0][0])  # 1
+print(matriz[0][-1])  # 2
+print(matriz[-1][-1])  # "c"
