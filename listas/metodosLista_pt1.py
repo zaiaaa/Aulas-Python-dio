fd114135d6f1beee41e640a60590bc566a816343
zaiaaa <gustavo.0801214@gmail.com>
2023-07-24 10:07:22 -0300

    os métodos append, count, copy, extend e clear

---
+++ listas/metodosLista_pt1.py
@@ -0,0 +1,35 @@
+# os métodos append, count, copy, extend e clear
+
+
+lista = []
+
+lista.append(1)
+lista.append("Python")
+lista.append([40, 30, 20])
+
+print(lista)
+
+# faz uma nova lista, copiando tudo o que tem na outra
+l2 = lista.copy()
+print(l2)
+
+# COUNT
+cores = ["azul", "verde", "roxo"]
+
+# retorna 0 (conta a posição do elemento pedido)
+print(cores.count("vermelho"))
+# ------------------------------------------------
+
+# EXTEND
+linguagens = ["python", "js", "c"]
+
+print(linguagens)  # ["python", "js", "c"]
+
+# basicamente você consegue adicionar mais de um item ao mesmo tempo na lista (NÃO RETORNA UMA LISTA DENTRO DA OUTRA)
+linguagens.extend(["java", "csharp"])
+
+print(linguagens)  # ["python", "js", "c", "java", "csharp"]
+# -----------------------------------------------------
+
+lista.clear()
+print(lista)
