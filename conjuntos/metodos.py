4e88e47342ccdcf09eb55db4103e5227cc637a55
zaiaaa <gustavo.0801214@gmail.com>
2023-08-02 18:43:50 -0300

    metodos para conjuntos

--- conjuntos/metodos.py
+++ conjuntos/metodos.py
@@ -10,7 +10,7 @@ conjunto_a2 = {1, 2, 3}
 conjunto_b2 = {2, 3, 4}
 
 resultado2 = conjunto_a2.intersection(conjunto_b2)
-print(resultado2)
+print(resultado2)  # {2, 3}
 
 # retorna as diferenças de um conjunto pro outro
 conjunto_a3 = {1, 2, 3}
@@ -38,3 +38,55 @@ print(resultado)
 
 resultado = conjunto_b5.issubset(conjunto_a5)  # False
 print(resultado)
+
+
+# pergunta de eles são interligados
+conjunto_a6 = {1, 2, 3, 4, 5}
+conjunto_b6 = {6, 7, 8, 9}
+conjunto_c = {1, 0}
+
+resultado = conjunto_a6.isdisjoint(conjunto_b6)  # True
+print(resultado)
+
+resultado = conjunto_b6.isdisjoint(conjunto_c)  # False
+print(resultado)
+
+
+# {}.add
+
+sorteio = {1, 23}
+
+sorteio.add(25)  # {1, 23, 25}
+sorteio.add(42)  # {1, 23, 25, 42}
+sorteio.add(25)  # {1, 23, 25, 42}
+# Ele não adiciona números que já existem
+
+# {}.copy
+sorteio.copy()
+# Só copia a lista
+
+# {}.discard
+sorteio.discard(1)
+# exclui algum valor que você queira excluir, caso você passe algum numero que não exista na lista, não ocorrerá nenhum erro
+
+# {}.pop
+sorteio.pop()
+# Exclui o primeiro item da lista
+
+# {}.remove
+sorteio.remove(0)
+# mesma coisa que o discard, mas acontece um erro quando você coloca um valor que não existe
+
+# len
+len(sorteio)
+# passa o tamanho da lista
+
+# in
+1 in sorteio  # true
+78912 in sorteio  # false
+# pergunta se tal coisa está dentro da lista
+
+
+# {}.clear
+sorteio.clear()
+# Só limpa a lista mesmo
